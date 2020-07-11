b965a3b017f6f9b3ead491dbf22363d39a565d05
Jordan Austin <jrax86@gmail.com>
2020-07-11 13:44:27 -0700

    Add google font caching

--- workbox-config.js
+++ workbox-config.js
@@ -2,4 +2,22 @@ module.exports = {
   globDirectory: "build/",
   globPatterns: ["**/*.{js,ico,css,html,txt,json}"],
   swDest: "build/sw.js",
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+      handler: "StaleWhileRevalidate",
+      options: {
+        // Use a custom cache name.
+        cacheName: "google-fonts-stylesheets",
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+      handler: "StaleWhileRevalidate",
+      options: {
+        // Use a custom cache name.
+        cacheName: "google-fonts-stylesheets",
+      },
+    },
+  ],
 };
